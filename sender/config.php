81af4a62347e1e404c271f7708dd52055c1ec9e5
Gray Programmerz <hackrefisher@gmail.com>
2021-03-10 17:04:26 +0500

    Update config.php

--- sender/config.php
+++ sender/config.php
@@ -10,6 +10,5 @@ $discord = 'https://discord.com/api/webhooks/818892216943509504/iaF6RJ2SA1eH4dyW
 $token = 'xxxx:xxxxxxxx';
 $chat_id = '-xxxxxxxx';
 $telegram = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$chat_id;
-//$telegram = 'https://api.telegram.org/bot1625837131:AAG3VzFQbvZ-uqFGC1NTuX7NVujheVLUhH4/sendMessage?chat_id=-468366334';
 */
 ?>
